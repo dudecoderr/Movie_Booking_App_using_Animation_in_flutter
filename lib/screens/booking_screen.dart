d721d7cc3ca5a9a4a9c7ae0bd12805b440969331
dudecoderr <dudecoderr@gmail.com>
2023-03-16 15:12:20 +0530

    [FIX] UI changes & error solve

--- lib/screens/booking_screen.dart
+++ lib/screens/booking_screen.dart
@@ -1,6 +1,5 @@
+import 'dart:math';
 import 'dart:ui';
-
-import 'package:animated_page_transition/animated_page_transition.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movie_booking_app/constant/horizontal_listview/date_picker.dart';
@@ -9,29 +8,94 @@ import 'package:movie_booking_app/screens/book_tickets_screen.dart';
 import 'package:movie_booking_app/screens/detail_screen.dart';
 import 'package:movie_booking_app/screens/time_picker.dart';
 
+class VideoClipper1 extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(0.0, size.height);
+    path.arcToPoint(Offset(size.width, size.height),
+        clockwise: true, radius: Radius.circular(size.width * 2));
+    path.lineTo(size.width, 33.0);
+    path.arcToPoint(const Offset(0.0, 33.0),
+        clockwise: false, radius: Radius.circular(size.width * 2));
+    path.close();
 
+    return path;
+  }
 
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
 
+// class VideoClipper extends CustomClipper<Path> {
+//   double curveValue;
+//
+//   VideoClipper({required this.curveValue});
+//
+//   @override
+//   Path getClip(Size size) {
+//     var path = Path();
+//     print("clip = " + curveValue.toString());
+//     path.lineTo(0, size.height / 4 * curveValue);
+//     path.quadraticBezierTo(
+//         size.width / 2, 0, size.width, size.height / 4 * curveValue);
+//     path.lineTo(size.width, size.height);
+//     path.lineTo(0, size.height);
+//     path.close();
+//     return path;
+//   }
+//
+//   @override
+//   bool shouldReclip(CustomClipper<Path> oldClipper) {
+//     return true;
+//   }
+// }
 class BookingScreen extends StatefulWidget {
   final String imageList;
   final String movieName;
-  const BookingScreen({Key? key, required this.imageList, required this.movieName}) : super(key: key);
+
+  const BookingScreen(
+      {Key? key, required this.imageList, required this.movieName})
+      : super(key: key);
 
   @override
   State<BookingScreen> createState() => _BookingScreenState();
 }
 
-class _BookingScreenState extends State<BookingScreen> with TickerProviderStateMixin {
+class _BookingScreenState extends State<BookingScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _cinemaScreenAc;
+  late Animation<double> _cinemaScreenTween;
+
+  @override
+  void initState() {
+    initializeAnimation();
+    super.initState();
+  }
+
+  void initializeAnimation() {
+    _cinemaScreenAc = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 2000));
+    _cinemaScreenTween = Tween<double>(begin: 0, end: 1)
+        .chain(CurveTween(curve: Curves.easeInOutQuart))
+        .animate(_cinemaScreenAc);
+    Future.delayed(Duration(milliseconds: 800), () {
+      _cinemaScreenAc.forward();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
-          height: 710.h,
+          // height: 710.h,
 
           /// =================== Background Image ===================
           decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage(widget.imageList), fit: BoxFit.fill),
+            image: DecorationImage(
+                image: AssetImage(widget.imageList), fit: BoxFit.fill),
           ),
 
           child: BackdropFilter(
@@ -50,14 +114,18 @@ class _BookingScreenState extends State<BookingScreen> with TickerProviderStateM
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => DetailScreen(imageList: widget.imageList, movieName: widget.movieName),
+                            builder: (context) => DetailScreen(
+                                imageList: widget.imageList,
+                                movieName: widget.movieName),
                           ),
                         );
                       },
                       child: Container(
                         height: 40.h,
                         width: 43.w,
-                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.sp), color: Colors.white54),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15.sp),
+                            color: Colors.white54),
                         child: const Icon(Icons.arrow_back_ios_new),
                       ),
                     ),
@@ -65,30 +133,49 @@ class _BookingScreenState extends State<BookingScreen> with TickerProviderStateM
 
                   /// =================== Day ===================
                   SizedBox(height: 5.h),
-                  Text('Day', style: TextStyle(color: Colors.white, fontSize: 15.sp, fontWeight: FontWeight.w600)),
+                  Text('Day',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.w600)),
                   SizedBox(height: 8.h),
                   const DatePicker(),
 
                   /// =================== Time ===================
                   SizedBox(height: 15.h),
-                  Text('Time', style: TextStyle(color: Colors.white, fontSize: 15.sp, fontWeight: FontWeight.w600)),
+                  Text('Time',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.w600)),
                   SizedBox(height: 8.h),
                   const TimeHorizontalListview(),
 
                   /// =================== Movie Image ===================
-                  SizedBox(height: 25.h),
+                  // SizedBox(height: 25.h),
 
-                  Container(
-                    height: 120.h,
-                    width: 300.w,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.sp),
-                      image: DecorationImage(image: AssetImage(widget.imageList), fit: BoxFit.fill),
+                  Transform(
+                    alignment: FractionalOffset.center,
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.002)
+                      ..rotateX(pi / 4)
+                      ..scale(0.9),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 17.0),
+                      child: AspectRatio(
+                        aspectRatio: 1.5,
+                        child: ClipPath(
+                            clipper: VideoClipper1(),
+                            child: Image.asset(
+                              widget.imageList,
+                              fit: BoxFit.fill,
+                            )),
+                      ),
                     ),
                   ),
 
                   /// =================== Select Seats ===================
-                  SizedBox(height: 20.h),
+                  // SizedBox(height: 15.h),
                   // First Seat Row
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -183,17 +270,22 @@ class _BookingScreenState extends State<BookingScreen> with TickerProviderStateM
                     child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.yellow.shade700,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.sp)),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.sp)),
                         ),
                         onPressed: () {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => BookTicketsScreen(imageList: widget.imageList, movieName: widget.movieName),
+                              builder: (context) => BookTicketsScreen(
+                                  imageList: widget.imageList,
+                                  movieName: widget.movieName),
                             ),
                           );
                         },
-                        child: Text('Book Tickets', style: TextStyle(color: Colors.black, fontSize: 15.sp))),
+                        child: Text('Book Tickets',
+                            style: TextStyle(
+                                color: Colors.black, fontSize: 15.sp))),
                   ),
                   SizedBox(height: 15.h),
                 ],
